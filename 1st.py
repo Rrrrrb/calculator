fbaa1a5090abe9add66a1ff4bcf6eb66f866568d
Rabia Batool <wtvidc105@gmail.com>
2024-10-03 17:09:32 +0500

    multiply and divide

--- 1st.py
+++ 1st.py
@@ -10,3 +10,12 @@ def divide(x, y):
     if y == 0:
         return "Error! Division by zero."
     return x / y
+def calculator():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    
+    choice = input("Enter choice (1/2/3/4): ")
+    
